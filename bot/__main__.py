f41978403f4fa6b32057affbd0e71f24196293c0
starry-shivam <starry@krsh.dev>
2026-02-04 09:48:22 +0530

    Increase live fetch update interval
    Signed-off-by: starry-shivam <starry@krsh.dev>

--- bot/__main__.py
+++ bot/__main__.py
@@ -178,7 +178,7 @@ def main():
 
     if LIVE_FETCH_IN_LOG:
         app.job_queue.run_repeating(
-            live_fastfetch, interval=60, first=5, job_kwargs={"misfire_grace_time": 10}
+            live_fastfetch, interval=300, first=10, job_kwargs={"misfire_grace_time": 10}
         )
 
     print("🤖 Bot is running…")
