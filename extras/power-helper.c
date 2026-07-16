ff3df30d52eaaec2b60224b8a1dff7638235a5aa
starry-shivam <touka.krs@gmail.com>
2026-07-16 20:06:15 +0530

    fix: minor fixes in power-helper and systemd adjustments
    Signed-off-by: starry-shivam <touka.krs@gmail.com>

--- extras/power-helper.c
+++ extras/power-helper.c
@@ -4,65 +4,60 @@
  */
 
 #include <errno.h>
-#include <grp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 enum {
-    ACTION_EXEC_FAILED = 0,
     ACTION_INVALID = -1,
 };
 
-static char *const REBOOT_ARGV[] = {"reboot", NULL};
-static char *const POWEROFF_ARGV[] = {"poweroff", NULL};
+static char *const REBOOT_ARGV[] = {
+    "reboot",
+    NULL,
+};
 
-static int run_action(const char *action) {
+static char *const POWEROFF_ARGV[] = {
+    "poweroff",
+    NULL,
+};
+
+static int run_action(const char *action, int *err) {
     if (strcmp(action, "reboot") == 0) {
-        if (execv("/usr/sbin/reboot", REBOOT_ARGV) == -1 &&
-            errno == ENOENT) {
+        execv("/usr/sbin/reboot", REBOOT_ARGV);
+
+        if (errno == ENOENT) {
             execv("/usr/bin/reboot", REBOOT_ARGV);
         }
-        return ACTION_EXEC_FAILED;
+        *err = errno;
+        return 0;
     }
 
     if (strcmp(action, "poweroff") == 0) {
-        if (execv("/usr/sbin/poweroff", POWEROFF_ARGV) == -1 &&
-            errno == ENOENT) {
+        execv("/usr/sbin/poweroff", POWEROFF_ARGV);
+
+        if (errno == ENOENT) {
             execv("/usr/bin/poweroff", POWEROFF_ARGV);
         }
-        return ACTION_EXEC_FAILED;
+        *err = errno;
+        return 0;
     }
 
     return ACTION_INVALID;
 }
 
 int main(int argc, char *argv[]) {
+    int err;
     int action_result;
 
     if (argc != 2) {
-        fprintf(stderr, "usage: %s <reboot|poweroff>\n", argv[0]);
+        fprintf(stderr, "Usage: power-helper <reboot|poweroff>\n");
         return EXIT_FAILURE;
     }
 
     if (geteuid() != 0) {
         fprintf(stderr, "power-helper must be installed setuid root\n");
-        return EXIT_FAILURE;
-    }
-
-    if (setgroups(0, NULL) != 0) {
-        perror("setgroups");
-        return EXIT_FAILURE;
-    }
-
-    if (setgid(0) != 0) {
-        perror("setgid");
-        return EXIT_FAILURE;
-    }
-
-    if (setuid(0) != 0) {
-        perror("setuid");
         return EXIT_FAILURE;
     }
 
@@ -72,13 +67,15 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    action_result = run_action(argv[1]);
+    err = 0;
+    action_result = run_action(argv[1], &err);
 
     if (action_result == ACTION_INVALID) {
         fprintf(stderr, "invalid action\n");
         return EXIT_FAILURE;
     }
 
-    perror("exec");
-    return (errno == ENOENT) ? 127 : EXIT_FAILURE;
+    errno = err;
+    perror("execv");
+    return (err == ENOENT) ? 127 : EXIT_FAILURE;
 }
